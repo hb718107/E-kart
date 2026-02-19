a117948919b8dcbe762d73adb436b11ce4133c9b
hb718107 <hb718107@gmail.com>
2026-02-19 17:02:03 +0500

    feat: redesign product detail view w/ modern UI & glassmorphism

--- src/app/container/product-details/product-details.ts
+++ src/app/container/product-details/product-details.ts
@@ -3,25 +3,36 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ProductLister } from '../../Models/Product';
 import { ProductList } from '../product-list/product-list';
 import { CommonModule } from '@angular/common';
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'product-details',
-  imports: [CommonModule,FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './product-details.html',
   styleUrl: './product-details.css',
 })
 export class ProductDetails {
-   @Input() productListComp : ProductList;
-  
-     product:ProductLister;
+  @Input() productListComp: ProductList;
 
-    ngOnInit(){
-      this.product = this.productListComp.selectedProduct;
-    }
+  product: ProductLister;
 
-    icons={
-      buy:faCartShopping
-    }
+  icons = {
+    buy: faCartShopping,
+    close: faTimes
+  };
 
+  ngOnInit() {
+    this.product = this.productListComp.selectedProduct;
+    // Disable background scrolling when modal is open
+    document.body.style.overflow = 'hidden';
+  }
+
+  ngOnDestroy() {
+    // Re-enable background scrolling when modal is closed
+    document.body.style.overflow = 'auto';
+  }
+
+  closeProductDetail() {
+    this.productListComp.selectedProduct = undefined;
+  }
 }
